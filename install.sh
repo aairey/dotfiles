b1ba0eccd19ca00a9a3560e82ca12782752e7782
Andy Airey <airey.andy@gmail.com>
2016-06-07 17:50:59 +0200

    move vim stuff to subdir

--- install.sh
+++ install.sh
@@ -14,9 +14,9 @@ ln -fs $DOTFILES_ROOT/.dotfiles/functions $DOTFILES_ROOT/.functions
 ln -fs $DOTFILES_ROOT/.dotfiles/local/share/fonts $DOTFILES_ROOT/.local/share/fonts
 ln -fs $DOTFILES_ROOT/.dotfiles/oh-my-zsh $DOTFILES_ROOT/.oh-my-zsh
 ln -fs $DOTFILES_ROOT/.dotfiles/profile $DOTFILES_ROOT/.profile
-ln -fs $DOTFILES_ROOT/.dotfiles/vimrc $DOTFILES_ROOT/.vimrc
-ln -fs $DOTFILES_ROOT/.dotfiles/vimrc-windows $DOTFILES_ROOT/.vimrc-windows
-ln -fs $DOTFILES_ROOT/.dotfiles/vim_runtime $DOTFILES_ROOT/.vim_runtime
+ln -fs $DOTFILES_ROOT/.dotfiles/vim/vimrc $DOTFILES_ROOT/.vimrc
+ln -fs $DOTFILES_ROOT/.dotfiles/vim/vimrc-windows $DOTFILES_ROOT/.vimrc-windows
+ln -fs $DOTFILES_ROOT/.dotfiles/vim/vim_runtime $DOTFILES_ROOT/.vim_runtime
 ln -fs $DOTFILES_ROOT/.dotfiles/zshrc $DOTFILES_ROOT/.zshrc
 
 # Reload fonts
