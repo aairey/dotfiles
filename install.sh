eacf532c991256056558d75bc3a107645416b67f
Andy Airey <airey.andy@gmail.com>
2016-06-07 22:53:57 +0200

    Fix symbolic links for directories

--- install.sh
+++ install.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
 
-cd "$(dirname "$0")/.."
-DOTFILES_ROOT=$(pwd -P)
- 
+INSTALLDIR=$(cd $(dirname "$0") && pwd)
+
+#if current dir is not '~/.dotfiles', complain and exit
+if [ "$HOME/.dotfiles" != $INSTALLDIR ]; then 
+    echo "dotfiles repo not cloned into $HOME/.dotfiles! Exiting ..."
+    exit 0
+fi
 
 # create symlinks
 echo "Creating symlinks ..."
-ln -fs $DOTFILES_ROOT/.dotfiles/aliases $DOTFILES_ROOT/.aliases
-ln -fs $DOTFILES_ROOT/.dotfiles/bash_logout $DOTFILES_ROOT/.bash_logout
-ln -fs $DOTFILES_ROOT/.dotfiles/bash_profile $DOTFILES_ROOT/.bash_profile
-ln -fs $DOTFILES_ROOT/.dotfiles/bashrc $DOTFILES_ROOT/.bashrc
-ln -fs $DOTFILES_ROOT/.dotfiles/bashrc_debian $DOTFILES_ROOT/.bashrc_debian
-ln -fs $DOTFILES_ROOT/.dotfiles/functions $DOTFILES_ROOT/.functions
-ln -fs $DOTFILES_ROOT/.dotfiles/local/share/fonts $DOTFILES_ROOT/.local/share/fonts
-ln -fs $DOTFILES_ROOT/.dotfiles/oh-my-zsh $DOTFILES_ROOT/.oh-my-zsh
-ln -fs $DOTFILES_ROOT/.dotfiles/profile $DOTFILES_ROOT/.profile
-ln -fs $DOTFILES_ROOT/.dotfiles/vim/vimrc $DOTFILES_ROOT/.vimrc
-ln -fs $DOTFILES_ROOT/.dotfiles/vim/vimrc-windows $DOTFILES_ROOT/.vimrc-windows
-ln -fs $DOTFILES_ROOT/.dotfiles/vim/vim_runtime $DOTFILES_ROOT/.vim_runtime
-ln -fs $DOTFILES_ROOT/.dotfiles/zshrc $DOTFILES_ROOT/.zshrc
+ln -fs $INSTALLDIR/aliases $HOME/.aliases
+ln -fs $INSTALLDIR/bash_logout $HOME/.bash_logout
+ln -fs $INSTALLDIR/bash_profile $HOME/.bash_profile
+ln -fs $INSTALLDIR/bashrc $HOME/.bashrc
+ln -fs $INSTALLDIR/bashrc_debian $HOME/.bashrc_debian
+ln -fs $INSTALLDIR/functions $HOME/.functions
+ln -fsn $INSTALLDIR/local/share/fonts $HOME/.local/share/fonts
+ln -fsn $INSTALLDIR/oh-my-zsh $HOME/.oh-my-zsh
+ln -fs $INSTALLDIR/profile $HOME/.profile
+ln -fs $INSTALLDIR/vim/vimrc $HOME/.vimrc
+ln -fs $INSTALLDIR/vim/vimrc-windows $HOME/.vimrc-windows
+ln -fsn $INSTALLDIR/vim/vim_runtime $HOME/.vim_runtime
+ln -fs $INSTALLDIR/zshrc $HOME/.zshrc
 
 # Reload fonts
 echo "Reloading fonts cache ..."
-fc-cache -f
+#fc-cache -f
