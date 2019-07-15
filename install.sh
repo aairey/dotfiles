216eb6e581f46c80ad079a19f7227480bc775102
Andy Airey <airey.andy@gmail.com>
2019-07-15 08:22:35 +0200

    Initial setup on MacOs

--- install.sh
+++ install.sh
@@ -20,13 +20,21 @@ pip3 install -r dotdrop/requirements.txt --user
 
 # create symlinks
 alias dotdrop="${CURR_DIR}/dotdrop.sh --cfg=${CURR_DIR}/dotdrop.yml"
-dotdrop install
+./dotdrop.sh --cfg ./dotdrop.yml install
+
+# Install zsh related things
+curl -sfL git.io/antibody | sh -s - -b /usr/local/bin
+go get gopkg.in/Netflix-Skunkworks/go-jira.v1/cmd/jira
+#brew install rust
+cargo install vivid
+#brew install ripgrep / sudo dnf install -y ripgrep 
+#brew install fd / sudo dnf install -y fd-find
 
 # install colorls, howdoi, asciinema, dockly, fzf, whereami, gtop
 sudo gem install colorls
 sudo pip install howdoi
 sudo python3 -m pip install asciinema
 sudo npm install -g dockly
-sudo dnf install fzf
+#sudo dnf install -y fzf
 sudo npm install -g @rafaelrinaldi/whereami
 sudo npm install -g gtop
